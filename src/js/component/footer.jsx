4e7dd2c098a24c2abdbe0be0a9590d7073601d28
jantgg <smosh360@hotmail.com>
2022-12-06 12:55:38 +0000

    arreglo

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () =>{
+    return (
+        <footer className="text-center text-center text-white mb-4" style="background-color: #3e4551">
+    <div className="container p-4 pb-0">
+        <section className="mb-4">
+        <p>Copyright @ Your Website 2019</p>
+        </section>
+    </div>          
+</footer>
+    );
+};
+export default Footer;
