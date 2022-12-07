0d12b9abc6ebeec627bf762225ce19f0f56de29b
KARENNGB <karengutierrezb@hotmail.com>
2022-12-07 15:45:39 +0000

    Footer modified

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 const Footer = () =>{
     return (
-        <footer className="text-center text-center text-white mb-4" style="background-color: #3e4551">
-    <div className="container p-4 pb-0">
-        <section className="mb-4">
-        <p>Copyright @ Your Website 2019</p>
-        </section>
+        <footer className="text-center text-white" style={{backgroundColor: "#3e4551"}}>
+    <div className="container p-4">
+        <p>{"Copyright @ Your Website 2019"}</p>
     </div>          
 </footer>
     );
