f7d045af372fc7d5ad53782b6bd37894ea78a0db
KARENNGB <karengutierrezb@hotmail.com>
2022-12-07 15:32:50 +0000

    card_modified_again

---
+++ src/js/component/cardKaren.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CardKaren = () => {
+    return (
+
+        <div className="col">
+          <div className="card text-center">
+            <img src="https://via.placeholder.com/500x325"/>
+            <div className="card-body">
+              <h2>Card Title</h2>
+              <p>Lorem ipssum sit amet, consectetur adipisicing elit. Sapiente esse, necessesitatibus neque.</p>
+              <button type="button" className="btn btn-primary">Find Out More!</button>
+            </div>
+          </div>
+        </div>
+    );
+}
+export default CardKaren;
